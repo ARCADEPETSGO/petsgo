15234a142f61628b2015b1fa560fb43aa0de911f
ARCADEPETSGO <cmdpixler+githuv@gmail.com>
2025-04-25 23:07:24 +0400

    Update petsgo.lua

--- petsgo.lua
+++ petsgo.lua
@@ -6,7 +6,8 @@ local Network = RS:WaitForChild("Network")
 -- Enter the arcade instance
 Network:WaitForChild("Instancing_PlayerEnterInstance"):InvokeServer("ArcadeInstance")
 
--- Fast loop using Heartbeat (no rebirth)
+-- Fast loop using Heartbeat (includes rebirth)
 game:GetService("RunService").Heartbeat:Connect(function()
     Network:WaitForChild("Instancing_FireCustomFromClient"):FireServer("ArcadeInstance", "Arcade_OnClick")
+    Network:WaitForChild("Arcade_RequestRebirth"):InvokeServer(50)
 end)
